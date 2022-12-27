4eddc0ab6a3dd6fdb2066b936749ab0361dec066
unknown <anna.ostman24@gmail.com>
2022-12-27 14:02:20 +0200

    NOW my own calculation should work

--- crosskey_codetest/src/main/java/org/example/LoanCalculator.java
+++ crosskey_codetest/src/main/java/org/example/LoanCalculator.java
@@ -4,21 +4,51 @@ public class LoanCalculator {
     private final ExchangeHandler exchangeHandler = new ExchangeHandler();
 
     public int calculateInterestRate(double interest, int months, int loan) {
-        double calculatedInterest = ((interest/100)/months) * loan; //We get the answer in euros, we exchange it to
+        int calculatedInterest = (int) (((interest/100)/months) * loan); //We get the answer in euros, we exchange it to
                                                                     //cents before sending it back
-        return exchangeHandler.eurosIntoCents(calculatedInterest);
+        return calculatedInterest;
     }
 
     public double calculateMonthlyRate(int loan, int months, double interest){
-        int result = ((loan / months) + calculateInterestRate(interest, months, loan)); //Maybe wrong?
-        return exchangeHandler.centsIntoEuros(result);
+        int result = loan / months;
+        int result2 = result + calculateInterestRate(interest, months, loan);
+        return exchangeHandler.centsIntoEuros(result2);
     }
 
-    /*
-    public void crosskeysFormula(int load, int months, double interest){
-        int result;
-        double
 
-    }*/
+    public double crosskeysFormula(double interest, int months, int loan){
+/*        int interestOnMonthlyBasis = calculateInterestRate(interest, months, loan);
+        int thePower = toThePowerOf(1+interestOnMonthlyBasis, months);
+        int a = interestOnMonthlyBasis * thePower;
+        int b = thePower -1;
+        double d = (double)a/b;
+        int c = exchangeHandler.eurosIntoCents(d);
+        int result = loan * c;
+        //System.out.println(exchangeHandler.centsIntoEuros(result));*/
+
+        double interestOnMonthlyBasis = exchangeHandler.centsIntoEuros(calculateInterestRate(interest, months, loan));
+        double thePower = toThePowerOf(1+interestOnMonthlyBasis, months);
+        double a = interestOnMonthlyBasis * thePower;
+        double b = thePower -1;
+        double d = a/b;
+        //int c = exchangeHandler.eurosIntoCents(d);
+        double result = (loan /100) * d;
+        //System.out.println(exchangeHandler.centsIntoEuros(result));v
+
+
+        return result;
+    }
+
+    public double toThePowerOf(double base, int months){
+        double result = 1;
+
+        for (int power = months; power != 0; power--) {
+            result = result * base;
+        }
+        System.out.println("Result =  " + result);
+
+        return result;
+
+    }
 
 }
--- crosskey_codetest/src/main/java/org/example/Main.java
+++ crosskey_codetest/src/main/java/org/example/Main.java
@@ -4,12 +4,15 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
         FileHandler fileHandler = new FileHandler();
         fileHandler.readFile();
         fileHandler.printFile();
 
-        /*
-        LoanCalculator test = new LoanCalculator();
-        System.out.println(test.calculateInterestRate(5, 12, 1000));*/
+
+        //LoanCalculator test = new LoanCalculator();
+        //System.out.println(test.crosskeysFormula(5, 24, 1000));
+
+
     }
 }
